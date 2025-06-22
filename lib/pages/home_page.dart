08983042cb53d1b5c8ff25c520b9c613389dddf1
sorawit <thankzaki@hotmail.com>
2025-06-22 23:46:34 +0700

    Step 3: make a FirestoreService and add data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,55 @@
 // Step 2: make a home page
 import 'package:flutter/material.dart';
+import 'package:learn_flutter4/services/firestore.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Step 3: make a FirestoreService
+  final FirestoreService firestoreService = FirestoreService();
+
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+  // open a dialog vox to add a note
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            content: TextField(controller: textController),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  // add a new note
+                  firestoreService.addNote(textController.text);
+
+                  // clear the text controller
+                  textController.clear();
+
+                  // close the box
+                  Navigator.pop(context);
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Notes")),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          openNoteBox();
+        },
         child: Icon(Icons.add),
       ),
     );
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,2 +1,20 @@
 // Step 3: make a FirestoreService
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  // Get collection of notes
+  final CollectionReference notes = FirebaseFirestore.instance.collection(
+    'notes',
+  );
+
+  // Create
+  Future<void> addNote(String note) {
+    return notes.add({'note': note, 'timestamp': Timestamp.now()});
+  }
+
+  // Read
+
+  // Update
+
+  // Delete
+}
